06d7bf51e8e61b95c11ad4d84c25bd8cd807139f
KrasniKot <emanueltrias9@gmail.com>
2024-01-19 09:47:00 -0800

    task 1

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -9,41 +9,41 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-        size_t level = 0, tree_height = 0;
-        heap_t *location = NULL, *new_node = NULL;
+	size_t level = 0, tree_height = 0;
+	heap_t *location = NULL, *new_node = NULL;
 
-        if (root == NULL)
-                return (NULL);
+	if (root == NULL)
+		return (NULL);
 
-        if (*root == NULL)
-        {
-                new_node = binary_tree_node(*root, value);
-                *root = new_node;
-                return (new_node);
-        }
+	if (*root == NULL)
+	{
+		new_node = binary_tree_node(*root, value);
+		*root = new_node;
+		return (new_node);
+	}
 
-        tree_height = height(*root);
+	tree_height = height(*root);
 
-        for (level = 0; level < tree_height; level++)
-        {
-                location = find_location(*root, level);
-                if (location != NULL)
-                        break;
-        }
+	for (level = 0; level < tree_height; level++)
+	{
+		location = find_location(*root, level);
+		if (location != NULL)
+			break;
+	}
 
-        new_node = binary_tree_node(location, value);
+	new_node = binary_tree_node(location, value);
 
-        if (!location->left)
-                location->left = new_node;
-        else
-                location->right = new_node;
+	if (!location->left)
+		location->left = new_node;
+	else
+		location->right = new_node;
 
-        while (new_node->parent && new_node->n > new_node->parent->n)
-        {
-                new_node = swap(root, new_node);
-        }
+	while (new_node->parent && new_node->n > new_node->parent->n)
+	{
+		new_node = swap(root, new_node);
+	}
 
-        return (new_node);
+	return (new_node);
 }
 
 /**
@@ -54,10 +54,10 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 size_t height(heap_t *root)
 {
-        if (root == NULL)
-                return (0);
+	if (root == NULL)
+		return (0);
 
-        return (1 + height(root->left));
+	return (1 + height(root->left));
 }
 
 /**
@@ -69,25 +69,25 @@ size_t height(heap_t *root)
  */
 heap_t *find_location(heap_t *root, size_t level)
 {
-        heap_t *location = NULL;
+	heap_t *location = NULL;
 
-        if (root == NULL)
-                return (NULL);
+	if (root == NULL)
+		return (NULL);
 
-        if (level == 0 && root->right == NULL)
-                return (root);
+	if (level == 0 && root->right == NULL)
+		return (root);
 
-        else if (level == 0)
-                return (NULL);
+	else if (level == 0)
+		return (NULL);
 
-        location = find_location(root->left, level - 1);
+	location = find_location(root->left, level - 1);
 
-        if (location != NULL)
-                return (location);
+	if (location != NULL)
+		return (location);
 
-        location = find_location(root->right, level - 1);
+	location = find_location(root->right, level - 1);
 
-        return (location);
+	return (location);
 }
 
 /**
@@ -99,53 +99,53 @@ heap_t *find_location(heap_t *root, size_t level)
  */
 heap_t *swap(heap_t **root, heap_t *new_node)
 {
-        int left = 0;
-        heap_t *temp = new_node->parent, *temp_r = temp->right, *temp_l = temp->left;
+	int left = 0;
+	heap_t *temp = new_node->parent, *temp_r = temp->right, *temp_l = temp->left;
 
-        if (new_node->parent->left == new_node)
-                left = 1;
+	if (new_node->parent->left == new_node)
+		left = 1;
 
-        new_node->parent->right = new_node->right;
+	new_node->parent->right = new_node->right;
 
-        if (new_node->right)
-                new_node->right->parent = new_node->parent;
+	if (new_node->right)
+		new_node->right->parent = new_node->parent;
 
-        new_node->parent->left = new_node->left;
+	new_node->parent->left = new_node->left;
 
-        if (new_node->left)
-                new_node->left->parent = new_node->parent;
+	if (new_node->left)
+		new_node->left->parent = new_node->parent;
 
-        if (left)
-        {
-                new_node->right = temp_r;
-                if (temp_r)
-                        temp_r->parent = new_node;
-        }
-        else
-        {
-                new_node->left = temp_l;
-                if (temp_l)
-                        temp_l->parent = new_node;
-        }
+	if (left)
+	{
+		new_node->right = temp_r;
+		if (temp_r)
+			temp_r->parent = new_node;
+	}
+	else
+	{
+		new_node->left = temp_l;
+		if (temp_l)
+			temp_l->parent = new_node;
+	}
 
-        new_node->parent = temp->parent;
+	new_node->parent = temp->parent;
 
-        if (temp->parent)
-        {
-                if (temp->parent->left == temp)
-                        temp->parent->left = new_node;
-                else
-                        temp->parent->right = new_node;
-        }
-        else
-                *root = new_node;
+	if (temp->parent)
+	{
+		if (temp->parent->left == temp)
+			temp->parent->left = new_node;
+		else
+			temp->parent->right = new_node;
+	}
+	else
+		*root = new_node;
 
-        if (left)
-                new_node->left = temp;
-        else
-                new_node->right = temp;
+	if (left)
+		new_node->left = temp;
+	else
+		new_node->right = temp;
 
-        temp->parent = new_node;
+	temp->parent = new_node;
 
-        return (new_node);
+	return (new_node);
 }
