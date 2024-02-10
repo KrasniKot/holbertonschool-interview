b2d171862c64102abb453faf76de2a2fecf2d56d
KrasniKot <emanueltrias9@gmail.com>
2024-02-09 17:13:17 -0800

    is palindrome implemented

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,55 +1,28 @@
+#include <stdbool.h>
+
 #include <stdio.h>
 #include "lists.h"
 
+int isPalindromeUtil(listint_t** left, listint_t* right) 
+{ 
+    if (right == NULL) 
+        return (1);
+  
+    bool isp = isPalindromeUtil(left, right->next);
 
-/**
- * is_palindrome - determines whether a linked list is palindrome:
- * @head: pointer to the first node of the linked list.
- *
- * Return: 0 if palindrome, 0 otherwise.
- */
+    if (isp == false) 
+        return (0); 
+ 
+    bool isp1 = (right->n == (*left)->n);
+
+    *left = (*left)->next;
+ 
+    return (isp1 ? 1: 0);
+} 
+ 
 int is_palindrome(listint_t **head)
 {
-    int l = len(*head);
-    int i;
-    listint_t *current = *head;
-    int array[l];
+    listint_t *h = *head;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    for (i = 0; i < l; i++)
-    {
-        array[i] = current->n;
-        current = current->next;
-    }
-
-    for (i = 0; i < l / 2; i++)
-    {
-        if (array[i] != array[l - i - 1])
-            return (0);
-    }
-
-    return (1);
-}
-
-
-/**
- * len - gets the length of a linked list.
- * @head: pointer to the first node of the linked list.
- * 
- * Return: linked list length.
- */
-int len(listint_t *head)
-{
-    int l = 0;
-    listint_t *c = head;
-
-    while (c)
-    {
-        l++;
-        c = c->next;
-    }
-
-    return (l);
+    return (isPalindromeUtil(&h, h));
 }
